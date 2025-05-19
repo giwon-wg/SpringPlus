bdd1ea0fbbd0a8bd6c09569df28ff18bf3672f7b
기원 <gi.won.git@gmail.com>
2025-05-19 14:01:13 +0900

    refa/store (#14)
    1. CRD 완료
    2. 커스텀 공통 예외 처리 완료
    3. 단건 조회와 전건 조회 DTO 분리
    4. `UserAuth` 추가 후 인가 부분 구현 예정
    5. `deleteStore` 부분 수정 필요

--- src/main/java/com/example/springplusteamproject/domain/store/controller/StoreController.java
+++ src/main/java/com/example/springplusteamproject/domain/store/controller/StoreController.java
@@ -1,16 +1,25 @@
 package com.example.springplusteamproject.domain.store.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springplusteamproject.common.response.ApiResponse;
 import com.example.springplusteamproject.domain.store.dto.request.StoreRequestDto;
+import com.example.springplusteamproject.domain.store.dto.response.StoreListResponseDto;
 import com.example.springplusteamproject.domain.store.dto.response.StoreResponseDto;
 import com.example.springplusteamproject.domain.store.service.StoreService;
 import com.example.springplusteamproject.domain.store.status.success.StoreSuccessCode;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +31,53 @@ public class StoreController {
 
     private final StoreService storeService;
 
+    @Operation(
+        summary = "가게 생성",
+        description = "가게를 생성합니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
     @PostMapping("/owner/stores")
     public ResponseEntity<ApiResponse<StoreResponseDto>> createStore(
         @Valid @RequestBody StoreRequestDto dto
     ) {
         StoreResponseDto responseDto = storeService.createStore(dto);
         return ApiResponse.onSuccess(StoreSuccessCode.STORE_ADD_SUCCESS, responseDto);
+    }
+
+    @Operation(
+        summary = "가게 폐업",
+        description = "가게를 폐업합니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @DeleteMapping("/owner/stores")
+    public ResponseEntity<ApiResponse<StoreResponseDto>> deletedStore(
+        // @AuthenticationPrincipal
+    ) {
+        // todo 수정 필요
+        storeService.deleteStore(1L);
+        return ApiResponse.onSuccess(StoreSuccessCode.STORE_DELETE_SUCCESS, null);
+    }
+
+    @Operation(
+        summary = "가게 조회",
+        description = "모든 가게를 조회합니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @GetMapping("/stores")
+    public ResponseEntity<ApiResponse<List<StoreListResponseDto>>> findStore() {
+        return ApiResponse.onSuccess(StoreSuccessCode.STORE_LOOKUP_SUCCESS, storeService.getAllStores());
+    }
+
+    @Operation(
+        summary = "가게 단건 조회",
+        description = "id를 기반으로 가게를 조회합니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @GetMapping("/stores/{storeId}")
+    public ResponseEntity<ApiResponse<StoreResponseDto>> getStoreById(
+        @PathVariable Long storeId
+    ) {
+        return ApiResponse.onSuccess(StoreSuccessCode.STORE_LOOKUP_SUCCESS, storeService.getStoreById(storeId));
     }
 
 }
