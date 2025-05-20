6e8f7e0e14d47073932b8fa8923ff768cfc978c0
rlqor9688 <31426528+rlqor9688@users.noreply.github.com>
2025-05-20 14:04:42 +0900

    [Docs] SecurityConfig 내 URL 검증 로직 추가, AuthUser 신규 생성 (#20)
    * [feature] : Auth/ Login 기능 일부 구현(임시 git 저장)
    
    * [feat/fix] : Auth/ Login 기능 일부 구현 (DTO Null 이슈 해결)
    
    1. JwtFilter :  token이 invalid할 때 다음 필터로 넘기는 로직 수정
    2. JwtUtil : Token이 유효하지 않을 때 어떤 에러인지 세부 로깅 추가
    3. UpdatePasswordRequestDto : @Setter, @JsonProperty Lombok 추가. DTO가 null로 인식되는 문제 해결을 위함.
    4. UserController : dto를 잘 읽어오는지 확인하기 위한 로그 추가
    
    * [feat] : Login 기능 중 유저 삭제 기능 구현
    
    1. UserController : deleteUser API 추가
    2. UserService : deleteMyAccount 메서드 추가
    3. UserServiceImpl : deleteMyAccount 메서드 구현
    
    * [docs] : User, ErrorCode 내 Consumer, Customer 혼용되는 상황을 Customer로 통일
    
    1. User : CONSUMER > CUSTOMER
    2. ErrorCode : CONSUMER > CUSTOMER
    
    * [fix/docs] : Swagger 참조 어노테이션 추가, 회원가입 로직 수정
    
    1. AuthController : @Operation 어노테이션 추가, ApiResponse 기본응답 추가
    2. AuthServiceImpl : AuthService interface 상속 / @Override 어노테이션 추가, 오너 가입 시 Brn 필수 입력 검증 로직 추가, 로그인 시 탈퇴 유저 여부 검증 로직 추가
    3. ErrorCode : BRN 미입력 시 에러코드 추가
    4. LoginRequestDto : @Schema 어노테이션 추가
    5. SignupRequestDto : @Schema 어노테이션 추가
    6. UpdatePasswordRequestDto : @Schema 어노테이션 추가
    7. UserController: ApiResponse(기본응답) 추가
    8. UserServiceImpl : 탈퇴 유저 검증 로직 추가
    9. AuthSuccessCode : AuthAPI 응답 시 성공 응답코드 설정
    10. UserSuccessCode : User API 응답 시 성공 응답코드 설정
    
    * [refac/docs] : ErrorCode > ErrorStatus / GlobalException > ApiException 통합
    
    1. 클래스 내 ErrorCode, GlobalException을 각각 ErrorStatus, ApiException으로 교체
    2. ErrorStatus 내 오류 코드 삽입
    
    * [docs] : UserSuccessCode, AuthSuccessCode > SuccessStatus로 병합
    
    1. SuccessStatus 내 성공 응답 코드 추가
    2. AuthController, UserController 내 성공 응답 코드 경로 수정
    
    * [docs] : ErrorCode, GlobalException, AuthSuccess, UserSuccessCode 삭제, Error/SuccessStatus 통합
    
    1. AuthServiceImpl : 디버깅용 에러 로그 추가, 에러코드 간소화
    2. ErrorStatus : 에러코드 간소화
    3. SuccessStatus : 성공코드 간소화
    4. UserController : 성공 응답 간소화
    5. UserServiceImpl : 디버깅용 에러 로그 추가
    
    * [docs] : 로그 내 비밀번호 숨김
    
    1. AuthServiceImpl : 로그 내 비밀번호를 남기지 않는 방향으로 수정
    2. UserServiceImpl : 로그 내 비밀번호를 남기지 않는 방향으로 수정
    
    * [docs] : SecurityConfig 내 URL 검증 로직 추가(UserRole), AuthUser 클래스 생성
    
    1. SecurityConfig 내 .requestMatchers("/owner/**").hasRole("OWNER") 설정 추가
    2. AuthUser : 테스트나 내부 호출 등에서 "인증된 유저 정보"를 대충 흉내 내기 위한 더미 객체 생성

--- src/main/java/com/example/springplusteamproject/security/SecurityConfig.java
+++ src/main/java/com/example/springplusteamproject/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         "/v3/api-docs/**",
                         "/swagger-resources/**",
                         "/webjars/**").permitAll()
+                .requestMatchers("/owner/**").hasRole("OWNER")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
