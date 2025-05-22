a694e88569b55df20e5d005edd17ab5d2ff02f34
기원 <gi.won.git@gmail.com>
2025-05-22 14:10:21 +0900

    분산 락 config 긴급 추가 (#53)
    * 분산 락 config 긴급 추가
    
    * 분산 락 config 수정
    
    ---------
    
    Co-authored-by: giwon-wg <giwon8766@naver.com>

---
+++ src/main/java/com/example/springplusteamproject/common/config/RedissonConfig.java
@@ -0,0 +1,28 @@
+package com.example.springplusteamproject.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+
+        Dotenv dotenv = Dotenv.load();
+
+        String redisHost = dotenv.get("REDIS_HOST", "localhost");
+        String redisPort = dotenv.get("REDIS_PORT", "6379");
+
+        Config config = new Config();
+
+        config.useSingleServer()
+            .setAddress("redis://" + redisHost + ":" + redisPort);
+        return Redisson.create(config);
+    }
+}
