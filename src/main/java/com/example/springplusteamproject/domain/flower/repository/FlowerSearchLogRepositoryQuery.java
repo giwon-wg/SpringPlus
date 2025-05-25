bfbb7cc7e1e4cacb05c30da5a9b610be2793809f
khg9900 <whale4100@gmail.com>
2025-05-26 08:59:38 +0900

    사용하지 않는 repository 삭제

--- src/main/java/com/example/springplusteamproject/domain/flower/repository/FlowerSearchLogRepositoryQuery.java
+++
@@ -1,9 +0,0 @@
-package com.example.springplusteamproject.domain.flower.repository;
-
-import com.example.springplusteamproject.domain.flower.dto.response.FlowerSearchResponseDto;
-import java.util.List;
-
-public interface FlowerSearchLogRepositoryQuery {
-
-    List<FlowerSearchResponseDto> getTop10Keywords(Integer year, Integer month, Integer day);
-}
