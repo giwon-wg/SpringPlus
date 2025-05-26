2f16a93313fe894b50acbd67a6ccc0a172aace9d
khg9900 <whale4100@gmail.com>
2025-05-26 09:02:15 +0900

    clean build

--- src/main/java/com/example/springplusteamproject/domain/flower/repository/FlowerSearchLogRepository.java
+++ src/main/java/com/example/springplusteamproject/domain/flower/repository/FlowerSearchLogRepository.java
@@ -3,6 +3,6 @@ package com.example.springplusteamproject.domain.flower.repository;
 import com.example.springplusteamproject.domain.flower.entity.FlowerSearchLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FlowerSearchLogRepository extends JpaRepository<FlowerSearchLog, Long>, FlowerSearchLogRepositoryQuery {
+public interface FlowerSearchLogRepository extends JpaRepository<FlowerSearchLog, Long> {
 
 }
